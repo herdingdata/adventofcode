e64de6cccfc7470b6d2aa40575768cc8136c6db7
Andy Herd <andy.herd@currenthealth.com>
2021-12-07 11:45:05 +0100

    day7 refactor parametrize and one function definition

--- y2021/day07/pesky_whales.py
+++ y2021/day07/pesky_whales.py
@@ -1,34 +1,26 @@
+from typing import List
+
 from y2021.helpers import read
 
 
-def least_fuel_required(crab_posns):
-    # pt1
-    posn_fuel = {}
-    for posn in range(0, max(crab_posns) + 1):
-        # in theory the position could be outside this range but let's gamble that it's not
-        fuel_rqd = sum([max(c, posn) - min(c, posn) for c in crab_posns])
-        posn_fuel[posn] = fuel_rqd
-    return min(posn_fuel.values())
-
-
-# pt2
-def how_much_fuel(from_pos, to_pos):
+def how_much_fuel(from_pos, to_pos, increase_burn_rate=False):
     burn_rate = 1
     fuel_consumed = 0
     step = -1 if to_pos < from_pos else 1
     for posn in range(from_pos, to_pos, step):
         # print(f"{posn=}, before {burn_rate=}, {fuel_consumed=}")
         fuel_consumed += burn_rate
-        burn_rate += 1
+        if increase_burn_rate:
+            burn_rate += 1
     return fuel_consumed
 
 
-def least_fuel_required_increase_burn_rate(crab_posns):
+def least_fuel_required(crab_posns: List[int], increase_burn_rate: bool):
     # this could be a lot faster but it worked so :shipitparrot:
     posn_fuel = {}
     for posn in range(0, max(crab_posns) + 1):
         # in theory the position could be outside this range but let's gamble that it's not
-        fuel_rqd = sum([how_much_fuel(from_pos=c, to_pos=posn) for c in crab_posns])
+        fuel_rqd = sum([how_much_fuel(from_pos=c, to_pos=posn, increase_burn_rate=increase_burn_rate) for c in crab_posns])
         posn_fuel[posn] = fuel_rqd
     return min(posn_fuel.values())
 
--- y2021/tests/test_day07.py
+++ y2021/tests/test_day07.py
@@ -1,39 +1,29 @@
+import pytest
+from collections import namedtuple
+
 from y2021.day07 import pesky_whales
 
 
 def test__least_fuel_required__their_sample__produces_expected():
     crab_posns = [16,1,2,0,4,2,7,1,2,14]
     expected_fuel = 37
-    result_fuel = pesky_whales.least_fuel_required(crab_posns)
+    result_fuel = pesky_whales.least_fuel_required(crab_posns, increase_burn_rate=False)
     assert result_fuel == expected_fuel
 
 
 def test__least_fuel_required_increase_burn_rate__their_sample__produces_expected():
     crab_posns = [16,1,2,0,4,2,7,1,2,14]
     expected_fuel = 168
-    result_fuel = pesky_whales.least_fuel_required_increase_burn_rate(crab_posns)
+    result_fuel = pesky_whales.least_fuel_required(crab_posns, increase_burn_rate=True)
     assert result_fuel == expected_fuel
 
 
-def test__how_much_fuel__from_0_to_5():
-    from_pos = 0
-    to_pos = 5
-    expected_fuel = 15
-    result_fuel = pesky_whales.how_much_fuel(from_pos, to_pos)
+@pytest.mark.parametrize("from_pos, to_pos, expected_fuel", [
+    (0, 5, 15),
+    (4, 5, 1),
+    (16, 5, 66),
+], ids=["from 0 to 5", "from 4 to 5", "from 16 to 5"])
+def test__how_much_fuel(from_pos, to_pos, expected_fuel):
+    result_fuel = pesky_whales.how_much_fuel(from_pos, to_pos, increase_burn_rate=True)
     assert result_fuel == expected_fuel
 
-
-def test__how_much_fuel__from_4_to_5():
-    from_pos = 4
-    to_pos = 5
-    expected_fuel = 1
-    result_fuel = pesky_whales.how_much_fuel(from_pos, to_pos)
-    assert result_fuel == expected_fuel
-
-
-def test__how_much_fuel__from_16_to_5():
-    from_pos = 16
-    to_pos = 5
-    expected_fuel = 66
-    result_fuel = pesky_whales.how_much_fuel(from_pos, to_pos)
-    assert result_fuel == expected_fuel
